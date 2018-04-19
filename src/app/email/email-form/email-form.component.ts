df58e54c91da7a13d11570f72d7d0168b0c8fc55
Zac <zacfirst@gmail.com>
2018-04-19 23:54:48 +1000

    feat: add email reactive form

--- src/app/email/email-form/email-form.component.ts
+++ src/app/email/email-form/email-form.component.ts
@@ -1,4 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { EmailApiService } from '../services/email-api.service';
+import { EmailResponse, EmailRequest } from '../models/email.model';
+
+interface EmailFormGroup extends FormGroup {
+  controls: {
+    from: FormControl;
+    to: FormControl;
+    cc: FormControl;
+    bcc: FormControl;
+    subject: FormControl;
+    body: FormControl;
+  };
+}
+
+interface EmailFormModel {
+  from: string[];
+  to: string[];
+  cc: string[];
+  bcc: string[];
+  subject: string;
+  body: string;
+}
+
+const EMAIL_REGULAR_EXPRESSION = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*;?$/
 
 @Component({
   selector: 'app-email-form',
@@ -6,10 +32,79 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./email-form.component.scss']
 })
 export class EmailFormComponent implements OnInit {
+  emailFormGroup: EmailFormGroup;
+  isSending: boolean;
+  isErrorShown: boolean;
+  errorMessage: string;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private emailApiService: EmailApiService,
+  ) { }
 
   ngOnInit() {
+    this.emailFormGroup = this.initEmailForm() as EmailFormGroup;
   }
 
+  submit(): void {
+    this.isSending = true;
+    const request = this.emailFormGroup.value as EmailRequest;
+    this.emailApiService
+      .send(request)
+      .finally(() => (this.isSending = false))
+      .subscribe(
+        (response: EmailResponse) => {
+          this.isErrorShown = false;
+        },
+        (error: HttpErrorResponse) => {
+          this.isErrorShown = true;
+          this.errorMessage = error.message;
+        },
+    );
+  }
+
+  private initEmailForm(): FormGroup {
+    return this.formBuilder.group({
+      from: [
+        {
+          value: 'zacfirst@gmail.com',
+          disabled: true
+        },
+        Validators.compose([
+          Validators.required
+        ]),
+      ],
+      to: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(EMAIL_REGULAR_EXPRESSION),
+        ]),
+      ],
+      cc: [
+        '',
+        Validators.compose([
+          Validators.pattern(EMAIL_REGULAR_EXPRESSION),
+        ]),
+      ],
+      bcc: [
+        '',
+        Validators.compose([
+          Validators.pattern(EMAIL_REGULAR_EXPRESSION),
+        ]),
+      ],
+      subject: [
+        '',
+        Validators.compose([
+          Validators.maxLength(78),
+        ]),
+      ],
+      body: [
+        '',
+        Validators.compose([
+          Validators.maxLength(1000),
+        ]),
+      ],
+    });
+  }
 }
--- src/app/email/email.module.ts
+++ src/app/email/email.module.ts
@@ -1,18 +1,23 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MultiEmailInputComponent } from './multi-email-input/multi-email-input.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { EmailFormComponent } from './email-form/email-form.component';
 import { EmailRoutingModule } from './email-routing.module';
+import { MultiEmailInputComponent } from './multi-email-input/multi-email-input.component';
 import { EmailApiService } from './services/email-api.service';
 
 @NgModule({
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     EmailRoutingModule,
     HttpClientModule,
   ],
-  declarations: [MultiEmailInputComponent, EmailFormComponent]
+  declarations: [
+    MultiEmailInputComponent,
+    EmailFormComponent
+  ],
   providers: [
     EmailApiService
   ]
