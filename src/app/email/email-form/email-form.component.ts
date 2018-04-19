6e82189046f7e95a9aa0935677bfc90258a29f54
Zac <zacfirst@gmail.com>
2018-04-19 21:33:22 +1000

    chore: enable sass option

--- src/app/email/email-form/email-form.component.ts
+++ src/app/email/email-form/email-form.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-email-form',
   templateUrl: './email-form.component.html',
-  styleUrls: ['./email-form.component.css']
+  styleUrls: ['./email-form.component.scss']
 })
 export class EmailFormComponent implements OnInit {
 
--- src/app/email/multi-email-input/multi-email-input.component.ts
+++ src/app/email/multi-email-input/multi-email-input.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-multi-email-input',
   templateUrl: './multi-email-input.component.html',
-  styleUrls: ['./multi-email-input.component.css']
+  styleUrls: ['./multi-email-input.component.scss']
 })
 export class MultiEmailInputComponent implements OnInit {
 
